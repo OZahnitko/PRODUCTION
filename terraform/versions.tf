a8902f480ac9ec8db62932c3a36045322d924bf6
sashaz <6964653+OZahnitko@users.noreply.github.com>
2021-12-06 12:23:44 +1000

    Infrastructure deploy on main - PRODUCTION.

---
+++ terraform/versions.tf
@@ -0,0 +1,19 @@
+terraform {
+  backend "s3" {
+    bucket = "my-bigg-state-buccc"
+    key    = "state/terraform.tfstate"
+    region = "us-east-1"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
